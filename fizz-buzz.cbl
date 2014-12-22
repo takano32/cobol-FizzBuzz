d538803e71371c9d009871da13c8ceb62b1524a2
Gatien Bovyn <gatien.bovyn@gmail.com>
2014-12-22 12:22:29 +0100

    Use variable for FIZZBUZZ strings

--- fizz-buzz.cbl
+++ fizz-buzz.cbl
@@ -3,31 +3,33 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 CT PIC 999 VALUE 1.
-       01 FZ PIC 999 VALUE 1.
-       01 BZ PIC 999 VALUE 1.
+       01 CT           PIC 999 VALUE 1.
+       01 FZ           PIC 999 VALUE 1.
+       01 BZ           PIC 999 VALUE 1.
+
+       01 FIZZBUZZ.
+           03 FIZZ     PIC X(4) VALUE "Fizz".
+           03 BUZZ     PIC X(4) VALUE "Buzz".
 
        PROCEDURE DIVISION.
        FIZZ-BUZZ-MAIN SECTION.
            PERFORM 100 TIMES
                IF FZ = 3 THEN
                    IF BZ = 5 THEN
-                       DISPLAY "FizzBuzz"
+                       DISPLAY FIZZBUZZ
                        COMPUTE BZ = 0
                    ELSE
-                       DISPLAY "Fizz"
+                       DISPLAY FIZZ
                    END-IF
                    COMPUTE FZ = 0
                ELSE
                    IF BZ = 5 THEN
-                       DISPLAY "Buzz"
+                       DISPLAY BUZZ
                        COMPUTE BZ = 0
                    ELSE
                        DISPLAY CT
                    END-IF
                 END-IF
-                ADD 1 TO CT
-                ADD 1 TO FZ
-                ADD 1 TO BZ
+                ADD 1 TO CT FZ BZ
            END-PERFORM
            STOP RUN.
